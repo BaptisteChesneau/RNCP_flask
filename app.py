db5cae8807c99186d723952269704afe3030a88b
Baptiste Chesneau <baptiste.chesneau@example.com>
2025-02-03 09:34:32 +0100

    Modification du fichier menu.html avec header, footer, navbar et carrousel

--- app.py
+++ app.py
@@ -2,10 +2,6 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-@app.route("/")
-def home():
-    return "Hello World!"
-
 @app.route("/menu")
 def menu():
     return render_template("menu.html")

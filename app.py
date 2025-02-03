10beffffa7903d0d54fa23777842c33b5d14e814
Baptiste Chesneau <baptiste.chesneau@example.com>
2025-02-03 09:31:44 +0100

    Ajout du fichier menu.html avec header, footer, navbar et carrousel

--- app.py
+++ app.py
@@ -1,11 +1,14 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route("/")
-def hello():
+def home():
     return "Hello World!"
 
+@app.route("/menu")
+def menu():
+    return render_template("menu.html")
+
 if __name__ == "__main__":
-    # Mode debug activé pour le développement
     app.run(debug=True)

e6a09e070505517cbdd3b6ed0030719fd9a50f24
Baptiste Chesneau <baptiste.chesneau@example.com>
2025-04-08 17:17:51 +0200

    Mise en place du modèle Utilisateur

--- app.py
+++ app.py
@@ -47,6 +47,21 @@ class Client(db.Model):
 
     def __repr__(self):
         return f"<Client {self.prenom} {self.nom}>"
+    
+class Utilisateur(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    nom_utilisateur = db.Column(db.String(100), unique=True, nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    mot_de_passe_hash = db.Column(db.String(200), nullable=False)
+
+    def __repr__(self):
+        return f"<Utilisateur {self.nom_utilisateur}>"
+
+    def set_password(self, password):
+        self.mot_de_passe_hash = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.mot_de_passe_hash, password)
 
 
 mail = Mail(app)

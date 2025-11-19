c6414584c161dde0472b3edcc241a054f1116ddb
Baptiste Chesneau <baptiste.chesneau@example.com>
2025-11-19 16:44:04 +0100

    I'd get contact

--- app.py
+++ app.py
@@ -673,9 +673,9 @@ def atelier():
 def nos_outils():
     return render_template("nos_outils.html")
 
-@app.route('/contact')
-def contact():
-    return render_template('contact.html')
+@app.route("/nous-contacter")
+def nous_contacter():
+    return render_template("contact.html")
 
 @app.route("/aide")
 def aide():

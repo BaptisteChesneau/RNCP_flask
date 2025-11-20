bce7e22748404a7529f8f53b11c87313f32086e6
Baptiste Chesneau <baptiste.chesneau@example.com>
2025-11-20 14:11:08 +0100

    change

--- app.py
+++ app.py
@@ -1448,14 +1448,6 @@ def add_security_headers(response):
 def page_not_found(e):
     return render_template("404.html"), 404
 
-@app.errorhandler(500)
-def internal_server_error(e):
-    return render_template("500.html"), 500
-
-@app.route("/forcetest500")
-def forcetest500():
-    raise Exception("Erreur forcée pour test 500")
-
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))

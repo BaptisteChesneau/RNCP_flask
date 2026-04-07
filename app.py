83c6a927d7e5374caf242309923714730ac28e31
Baptiste Chesneau <baptiste.chesneau@example.com>
2026-04-07 15:41:36 +0200

    change

--- app.py
+++ app.py
@@ -1256,12 +1256,12 @@ def admin_dashboard():
         last_question=last_question,
     )
 
-@app.errorhandler(404)
-def page_not_found(e):
+@app.route('/test-404')
+def test_404():
     return render_template('404.html'), 404
 
-@app.errorhandler(500)
-def internal_error(e):
+@app.route('/test-500')
+def test_500():
     return render_template('500.html'), 500
 
 @app.route("/ajouter-message", methods=["GET", "POST"])

390e6a521f1566a48c90adf75a4508d71419a5ed
Baptiste Chesneau <baptiste.chesneau@example.com>
2026-07-27 16:03:55 +0200

    change

--- app.py
+++ app.py
@@ -309,6 +309,12 @@ def compte_client():
     )
     return render_template("compte_client.html", clients=clients, devis_list=devis_list)
 
+@app.route("/parametres", methods=["GET", "POST"])
+def parametres():
+    if "utilisateur_id" not in session:
+        return redirect(url_for("login"))
+    return render_template("parametres.html")
+
 @app.route("/paiement")
 def paiement():
     if "utilisateur_id" not in session:

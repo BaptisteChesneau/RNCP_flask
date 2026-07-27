de2e018aedad63f8ec346915b196929b584f7285
Baptiste Chesneau <baptiste.chesneau@example.com>
2026-07-27 15:36:13 +0200

    change

---
+++ models/__init__.py
@@ -0,0 +1,4 @@
+from models.user import Utilisateur, ParametresCompte, Preferences
+from models.client import Client, UtilisateurClient
+from models.devis import Devis, Paiement
+from models.support import SupportTicket, Historique, Newsletter, BlogPost
---
+++ tests/conftest.py
@@ -0,0 +1,14 @@
+import pytest
+from app import app, db
+
+@pytest.fixture(scope="module")
+def client():
+    app.config["TESTING"] = True
+    app.config["WTF_CSRF_ENABLED"] = False
+
+    with app.test_client() as client:
+        with app.app_context():
+            db.create_all()
+            yield client
+            db.session.remove()
+            db.drop_all()

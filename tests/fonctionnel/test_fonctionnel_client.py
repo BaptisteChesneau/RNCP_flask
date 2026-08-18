0d2b7e0489dbbbc80556246eaa906c68c6d85b52
Baptiste Chesneau <baptiste.chesneau@example.com>
2026-08-18 16:43:51 +0200

    change

--- tests/fonctionnel/test_fonctionnel_client.py
+++ tests/fonctionnel/test_fonctionnel_client.py
@@ -6,19 +6,18 @@ def test_acces_espace_client_non_connecte():
     app = create_app()
     client = app.test_client()
     
-    # Tentative d'accès à la route /espace-client
     response = client.get('/compte-client', follow_redirects=True)
     
-    # Vérifie qu'on est redirigé vers la page de connexion
+    
     assert response.status_code == 200
-    assert b"Connexion" in response.data # Vérifie que le mot 'Connexion' est dans la page
+    assert b"Connexion" in response.data 
 
 def test_soumission_formulaire_devis():
     """Vérifie le bon fonctionnement du formulaire de demande de devis."""
     app = create_app()
     client = app.test_client()
     
-    # Simulation de la soumission d'un formulaire
+    
     response = client.post('/devis', data={
         'nom_entreprise': 'ML2C_TEST',
         'montant_estime': '1500'
